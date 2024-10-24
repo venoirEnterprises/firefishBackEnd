277c69b20c3c7c76678f00e6123ff4ae88ed2331
vnorris <nvsibeeyt@gmail.com>
2024-10-24 19:56:43 +0100

    Add project files.

---
+++ firefishBackEnd/Models/Candidate.cs
@@ -0,0 +1,10 @@
+﻿namespace firefishBackEnd.Models
+{
+    public class Candidate
+    {
+        public required string firstName { get; set; }
+        public required string surname { get; set; }
+        public required string dateOfBirth { get; set; }
+        public required string skills { get; set; }
+    }
+}
---
+++ firefishBackEnd/Services/CandidateService.cs
@@ -0,0 +1,49 @@
+﻿using firefishBackEnd.Models;
+using System.Data.SqlClient;
+using System.Reflection.Metadata.Ecma335;
+
+namespace firefishBackEnd.Services
+{
+    public class CandidateService
+    {
+
+        SqlConnection conn = new SqlConnection("Data Source=.\\SQLEXPRESS;Database=Web_API_Task;Integrated Security=True;Connect Timeout=30;Encrypt=False;");
+
+
+        public List<Candidate> GetCandidates()
+        {
+            List<Candidate> candidates = new List<Candidate>();
+
+            conn.Open();
+
+            using (SqlCommand command = new SqlCommand(@"SELECT 
+                   DISTINCT ID,FirstName,Surname,DateOfBirth,
+                    ISNULL(stuff((
+                    SELECT ',' + s2.name
+                    FROM Skill s2
+		            JOIN CandidateSkill cs ON cs.SkillID = s2.ID
+                    WHERE cs.CandidateID = c.ID
+                    ORDER BY s2.Name
+                    FOR xml path('')
+                ),1,1,''),'') as Skills
+                from Candidate c
+                GROUP BY ID,FirstName,Surname,DateOfBirth
+                ORDER BY ID ASC", conn))
+            {
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read()) {
+                    candidates.Add(new Candidate() { 
+                        firstName = reader.GetString(1), 
+                        surname = reader.GetString(2), 
+                        dateOfBirth = Convert.ToDateTime(reader["DateOfBirth"]).ToString("dd/MM/yyyy"),
+                        skills = reader.GetString(4)
+                    });
+                }
+            }
+
+            conn.Close();
+            return candidates;
+        }
+    }
+
+}
